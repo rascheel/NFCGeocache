26b3ce7e63f453a317e1a90d6d3af6b182f0d070
Ryan Scheel <scheel.ryan@gmail.com>
2012-12-03 19:57:09 -0600

    Updated profile activity

--- App/src/com/NFCGeo/ViewProfileActivity.java
+++ App/src/com/NFCGeo/ViewProfileActivity.java
@@ -3,14 +3,31 @@ package com.NFCGeo;
 import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
-//import android.widget.TextView;
+import android.widget.TextView;
 
 public class ViewProfileActivity extends Activity
 {
+    private TextView usernameBox;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_profile);
+
+        usernameBox = (TextView) findViewById(R.id.userName);
+        
+        if(MainMenu.user.type() == UserType.USER)
+        {
+            usernameBox.setText(MainMenu.user.name() + "'s Profile");
+        }
+        else if(MainMenu.user.type() == UserType.MODERATOR)
+        {
+            usernameBox.setText(MainMenu.user.name() + "'s Profile ---- MODERATOR");
+        }
+        else
+        {
+            usernameBox.setText(MainMenu.user.name() + "'s Profile ---- ADMIN");
+        }
     }
 }
